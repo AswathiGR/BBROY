45c5f51de8aa56c738cf1f4909a15e37eb04add4
AswathiGR <44152778+AswathiGR@users.noreply.github.com>
2021-07-03 02:28:08 +0530

    Add files via upload

---
+++ Color_Code/routes.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='/static')
+
+# two decorators, same function
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/Colorcode.html')
+def Colorcode():
+    return render_template('Colorcode.html', the_title='GET THE CODE!') 
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
